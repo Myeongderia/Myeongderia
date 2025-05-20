bc3b378ea4fd4fbb2925ce1053b95adc9f9f62dd
honghyobin <w2234@e-mirim.hs.kr>
2025-05-20 09:25:17 +0900

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(301, 139);
+            button1.Location = new Point(41, 32);
             button1.Name = "button1";
             button1.Size = new Size(213, 95);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(41, 167);
+            button2.Name = "button2";
+            button2.Size = new Size(213, 95);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -54,5 +65,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
